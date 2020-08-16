d0b70d2f4ccd58c7e786279bd859bbc6ba0b1ebd
zhaoxusun <30275668+zhaoxusun@users.noreply.github.com>
2020-08-16 08:35:48 +0800

    Update Test.java

--- src/test/java/file/Test.java
+++ src/test/java/file/Test.java
@@ -1,19 +1,18 @@
 package file;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
+import org.testng.annotations.Test;
 
 /**
  * Created by zhaoxu on 2017/7/7.
  */
-public class Test {
+public class Test1 {
 
-    @org.testng.annotations.Test
+    @Test
     public void testFileUtil(){
         FileFactory fileFactory = new FileFactory();
         //tool's entry
-        fileFactory.transformFile("test.xls","test4.xlsxv");
+        fileFactory.transformFile("test.xls","test222.json");
     }
 }
 
